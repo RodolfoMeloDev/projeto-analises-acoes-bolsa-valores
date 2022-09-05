add77d57dcb65900a2d3971b7b4d5bf80db32540
Rodolfo Lopes de Melo <rodolfo.melo.dev@gmail.com>
2022-09-04 21:36:25 -0300

    Alterado propriedade Status para Ativo na classe base

--- backend/src/App.Domain/Entities/BaseEntity.cs
+++ backend/src/App.Domain/Entities/BaseEntity.cs
@@ -5,19 +5,13 @@ namespace App.Domain.Entities
 {
     public abstract class BaseEntity
     {
-        const string STATUS_ATIVO = "A";
-        private string _status;
         private DateTime? _dataCadastro;
 
         [Key]
         public int Id { get; set; } 
 
-        [Required]
-        [MaxLength(1)]
-        public string Status { 
-            get { return _status; }
-            set { _status = value == null ? STATUS_ATIVO : value; } 
-        }
+        [Display(Name ="Status")]
+        public bool Ativo { get; set; }
 
         [Required]
         public DateTime? DataCadastro { 
