2c041bc6e6b1a892c899848d5042cec5b6e3940c
Rodolfo <brq.rodolfo@ao3tech.com>
2022-09-09 14:46:50 -0300

    Ajustado nome da Coluna Status para Ativo

--- backend/src/App.Domain/Entities/BaseEntity.cs
+++ backend/src/App.Domain/Entities/BaseEntity.cs
@@ -9,20 +9,21 @@ namespace App.Domain.Entities
         private DateTime? _dataAlteracao;
 
         [Key]
-        public int Id { get; set; } 
+        public int Id { get; set; }
 
-        [Display(Name ="Status")]
         public bool Ativo { get; set; }
 
         [Required]
-        public DateTime? DataCadastro { 
-            get { return _dataCadastro; } 
-            set { _dataCadastro = (value == null ? DateTime.UtcNow : value); } 
-            }
+        public DateTime? DataCadastro
+        {
+            get { return _dataCadastro; }
+            set { _dataCadastro = (value == null ? DateTime.UtcNow : value); }
+        }
 
-        public DateTime? DataAlteracao { 
+        public DateTime? DataAlteracao
+        {
             get { return _dataAlteracao; }
-            set { _dataAlteracao = (value == DateTime.MinValue ? null : value); } 
+            set { _dataAlteracao = (value == DateTime.MinValue ? null : value); }
         }
     }
 }
