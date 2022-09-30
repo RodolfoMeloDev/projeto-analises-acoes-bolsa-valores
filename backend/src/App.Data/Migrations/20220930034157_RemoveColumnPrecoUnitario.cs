ad1bff68c0f211899f71d713e450f0302fce1b73
Rodolfo Lopes de Melo <rodolfo.melo.dev@gmail.com>
2022-09-30 00:43:01 -0300

    Gerada migrations para remoção da coluna Preço Unitário

---
+++ backend/src/App.Data/Migrations/20220930034157_RemoveColumnPrecoUnitario.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace App.Data.Migrations
+{
+    public partial class RemoveColumnPrecoUnitario : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PrecoUnitario",
+                table: "HistoryTickers");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "PrecoUnitario",
+                table: "HistoryTickers",
+                type: "numeric",
+                nullable: false,
+                defaultValue: 0m);
+        }
+    }
+}
