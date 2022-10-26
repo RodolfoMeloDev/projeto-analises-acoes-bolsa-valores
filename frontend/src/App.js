dbf1dd923c9f5b0e870f2fb88da41867457172db
Rodolfo <brq.rodolfo@ao3tech.com>
2022-10-26 15:05:56 -0300

    Iniciando Projeto FrontEnd

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Menu from './components/Menu/Menu';
+
+function App() {
+  return (
+    <div className="App">
+      <Menu/>
+    </div>
+  );
+}
+
+export default App;
